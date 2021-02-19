404051be3c08bb2034802b0713117712dec55ad8
sh0seo <ssh0702@gmail.com>
2021-02-19 11:08:27 +0900

    chating

--- src/chat/App.tsx
+++ src/chat/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StatusBar, Image} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import SplashScreen from 'react-native-splash-screen';
 import {theme} from './theme';
 
 const App = () => {
-  SplashScreen.hide();
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
